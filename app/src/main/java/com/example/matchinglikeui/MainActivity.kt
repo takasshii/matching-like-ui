bfb5442ac92eff38bc487c7dbe5c3f4d609b9fbf
たかっしー <takashiho0913@gmail.com>
2022-06-12 20:22:40 +0900

    [add] bottomNavでの画面遷移を追加

--- app/src/main/java/com/example/matchinglikeui/MainActivity.kt
+++ app/src/main/java/com/example/matchinglikeui/MainActivity.kt
@@ -3,23 +3,25 @@ package com.example.matchinglikeui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Call
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.matchinglikeui.ui.discover.DiscoverScreen
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.matchinglikeui.navigation.BottomNavItem
+import com.example.matchinglikeui.navigation.NavigationGraph
 import com.example.matchinglikeui.ui.theme.MatchingLikeUITheme
 
 class MainActivity : ComponentActivity() {
@@ -42,11 +44,12 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun HomeScreen() {
+    val navController = rememberNavController()
     Scaffold(
         topBar = { TopAppBarScreen() },
-        bottomBar = {BottomNavigationBar()}
-    ) {
-        DiscoverScreen()
+        bottomBar = { BottomNavigationBar(navController = navController) }
+    ) { innerPadding ->
+        NavigationGraph(navController = navController, modifier = Modifier.padding(innerPadding))
     }
 }
 
@@ -74,8 +77,53 @@ fun TopAppBarScreen() {
 }
 
 @Composable
-fun BottomNavigationBar() {
+fun BottomNavigationBar(navController: NavController) {
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.Discover,
+        BottomNavItem.Chat,
+        BottomNavItem.MyPage,
+    )
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colors.background,
+        contentColor = Color.Black
+    ) {
+        //navの状態を取得できる
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
 
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        painter = painterResource(id = item.icon),
+                        contentDescription = item.title
+                    )
+                },
+                selected = currentDestination?.hierarchy?.any { it.route == item.screen_route } == true,
+                label = {
+                    Text(
+                        text = item.title,
+                        fontSize = 9.sp
+                    )
+                },
+                selectedContentColor = Color.Black,
+                unselectedContentColor = Color.Black.copy(0.4f),
+                alwaysShowLabel = true,
+                onClick = {
+                    navController.navigate(item.screen_route) {
+                        navController.graph.startDestinationRoute?.let { screen_route ->
+                            popUpTo(screen_route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
 }
 
 
--- app/src/main/java/com/example/matchinglikeui/navigation/BottomNavItem.kt
+++ app/src/main/java/com/example/matchinglikeui/navigation/BottomNavItem.kt
@@ -1,11 +1,11 @@
-package com.example.matchinglikeui.domain
+package com.example.matchinglikeui.navigation
 
 import com.example.matchinglikeui.R
 
 
-sealed class BottomNavItem(val title:String, var Icon:Int, val screen_route: String) {
+sealed class BottomNavItem(val title:String, var icon:Int, val screen_route: String) {
     object Home: BottomNavItem("Home", R.drawable.ic_outline_home_24, "home")
     object Discover: BottomNavItem("Discover", R.drawable.ic_outline_explore_24, "Discover")
-    object Chatting: BottomNavItem("Chatting", R.drawable.ic_outline_sms_24, "Chatting")
+    object Chat: BottomNavItem("Chat", R.drawable.ic_outline_sms_24, "Chat")
     object MyPage: BottomNavItem("MyPage", R.drawable.ic_outline_account_circle_24, "MyPage")
 }
